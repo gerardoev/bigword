9d4e8ba5648cbd059dd498d51c3db501d88649c9
Gerardo Erick Villa Aguilar <gerardoerick@outlook.com>
2020-11-04 13:33:55 -0600

    Estructura logeado-nologeado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import HomePage from "./components/HomePage/HomePage";
 
 function App() {
+  const [usuario, setUsuario] = useState(false);
   return (
     <div className="App">
-      <HomePage/>
+      {usuario ? <h1>Loageado</h1>: <HomePage/>}
     </div>
   );
 }
