a39dba51cccd0847e962ab3943dd074b2c1b4353
Gerardo Erick Villa Aguilar <gerardoerick@outlook.com>
2020-11-04 14:00:33 -0600

    comportamiento de los botones home

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,19 +3,19 @@ import {Jumbotron, Button, Col, Row} from 'reactstrap';
 import "./HomePage.scss";
 
 
-const Header = () => {
+const Header = ({toggleModalSignIn, toggleModalSignUp}) => {
     return (
         <Jumbotron>
                 <h1 className="logo">bigword</h1>
                 <h4 class="slogan">Menten un registro de tu vocabulario</h4>
                 <Row>
                     <Col xs="12" md="6">
-                        <Button id="btnIngresar">
+                        <Button id="btnIngresar" onClick={toggleModalSignIn}>
                             Ingresar
                         </Button>
                     </Col>
                     <Col xs="12" md="6">
-                        <Button>
+                        <Button onClick={toggleModalSignUp}>
                             Registro
                         </Button>
                     </Col>
@@ -26,10 +26,26 @@ const Header = () => {
 
 
 class HomePage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            showModalSignUp : false,
+            showModalSignIn : false,
+        }
+        this.toggleModalSignIn = this.toggleModalSignIn.bind(this)
+        this.toggleModalSignUp = this.toggleModalSignUp.bind(this)
+    }
+
+    toggleModalSignUp(){
+        this.setState({showModalSignUp: !this.state.showModalSignUp})
+    }
+    toggleModalSignIn(){
+        this.setState({showModalSignIn: !this.state.showModalSignIn})
+    }
 
     render() {
         return (
-            <Header/>
+            <Header toggleModalSignIn={this.toggleModalSignIn} toggleModalSignUp={this.toggleModalSignUp}/>
         );
     }
 }
