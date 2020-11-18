68e6163a3e7f11a86f6eab42d4fe1b7216fcb14b
Gerardo Erick Villa Aguilar <gerardoerick@outlook.com>
2020-11-17 20:34:46 -0600

    Formularios de registro y login

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -31,23 +31,89 @@ const FormularioLogin = () =>{
             <FormGroup>
                 <Row>
                     <Col xs={12} md={2}>
-                        <Label htmlFor="nombre">Nombre</Label>
+                        <Label htmlFor="nombre">Correo</Label>
                     </Col>
                     <Col xs={12} md={10}>
-                        <Input type="text" placeholder="Nombre"/>
+                        <Input type="email" placeholder="Correo" name="correo" id="correo"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={2}>
+                        <Label htmlFor="nombre">Contraseña</Label>
+                    </Col>
+                    <Col xs={12} md={10}>
+                        <Input type="password" placeholder="Contraseña" name="password" id="password"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="offset-md-8" xs={12} md={4}>
+                        <Button className="d-block w-100 btn-form-modal">Entrar</Button>
                     </Col>
                 </Row>
             </FormGroup>
         </Form>
     );
-}
+};
+
+const FormularioSignUp = () =>{
+    return(
+        <Form>
+            <FormGroup>
+                <Row>
+                    <Col xs={12} md={2}>
+                        <Label htmlFor="nombre">Nombre</Label>
+                    </Col>
+                    <Col xs={12} md={4}>
+                        <Input type="text" placeholder="Nombre" name="nombre" id="nombre"/>
+                    </Col>
+                    <Col xs={12} md={2}>
+                        <Label htmlFor="nombre">Apellidos</Label>
+                    </Col>
+                    <Col xs={12} md={4}>
+                        <Input type="text" placeholder="Apellidos" name="apellidos" id="apellidos"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={2}>
+                        <Label htmlFor="nombre">Correo</Label>
+                    </Col>
+                    <Col xs={12} md={10}>
+                        <Input type="email" placeholder="Correo" name="correo" id="correo"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={2}>
+                        <Label htmlFor="contraseña">Contraseña</Label>
+                    </Col>
+                    <Col xs={12} md={4}>
+                        <Input type="password" placeholder="Contraseña" name="contraseña" id="contraseña"/>
+                    </Col>
+                    <Col xs={12} md={2}>
+                        <Label htmlFor="repeat_contraseña">Repetir Contraseña</Label>
+                    </Col>
+                    <Col xs={12} md={4}>
+                        <Input type="password" placeholder="Repetir contraseña" name="repeat_contraseña" id="repeat_contraseña"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="col-xs-6">
+                        <Button className="d-md-block w-100">Cancelar</Button>
+                    </Col>
+                    <Col className="col-xs-6">
+                        <Button className="d-md-block w-100">Registrarse</Button>
+                    </Col>
+                </Row>
+            </FormGroup>
+        </Form>
+    );
+};
 
 class HomePage extends Component {
     constructor(props){
         super(props);
         this.state = {
-            showModalSignUp : false,
-            showModalSignIn : true,
+            showModalSignUp : true,
+            showModalSignIn : false,
         }
         this.toggleModalSignIn = this.toggleModalSignIn.bind(this)
         this.toggleModalSignUp = this.toggleModalSignUp.bind(this)
@@ -62,15 +128,15 @@ class HomePage extends Component {
 
     render() {
         return (
-            <>
+            <div className="home-page">
                 <Header toggleModalSignIn={this.toggleModalSignIn} toggleModalSignUp={this.toggleModalSignUp}/>
                 <BasicModal showModal={this.state.showModalSignUp} toggleModal={this.toggleModalSignUp} headerString = "SignUp">
-                    SighUpModal
+                    <FormularioSignUp/>
                 </BasicModal>
                 <BasicModal showModal={this.state.showModalSignIn} toggleModal={this.toggleModalSignIn} headerString = "SignIn">
                     <FormularioLogin/>
                 </BasicModal>
-            </>
+            </div>
         );
     }
 }
