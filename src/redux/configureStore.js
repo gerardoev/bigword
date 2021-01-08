eb46c24ac06c39fd31677ba60baca88156086e53
Gerardo Erick Villa Aguilar <gerardoerick@outlook.com>
2021-01-08 14:46:34 -0600

    Redux logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,15 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import {Categorias} from "./categorias";
 import {Palabras} from "./palabras";
+import { logger } from "redux-logger";
 
 export const ConfigureStore = () => {
     const store = createStore(
         combineReducers({
             categorias: Categorias,
             palabras: Palabras
-        })
+        }),
+        applyMiddleware(logger)
     );
     return store;
 }
