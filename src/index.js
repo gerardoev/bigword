827306246fb32bbbc73771f863f6f56828df6c79
Gerardo Erick Villa Aguilar <gerardoerick@outlook.com>
2021-05-07 20:17:36 -0500

    adding mdbootstrap for styled input file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "video-react/dist/video-react.css";
 import './index.scss';
