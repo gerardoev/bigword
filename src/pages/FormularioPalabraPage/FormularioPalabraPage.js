0e9a9bc64ec6dabe50d6728935a650e144d51349
Gerardo Erick Villa Aguilar <gerardoerick@outlook.com>
2021-07-13 20:35:17 -0500

    controlar formulario

--- src/pages/FormularioPalabraPage/FormularioPalabraPage.js
+++ src/pages/FormularioPalabraPage/FormularioPalabraPage.js
@@ -19,14 +19,51 @@ const FormularioPalabraPage = (props) => {
     //comprobamos si hay una imagen subida al backend y la ponemos como default. -> si estado de app dice que hau url: `http://geturldeservidor/`
     const [urlImagen, setUrlImagen] = useState( 
         false ? `` : null
-     );
+    );
     const [palabra, setPalabra] = useState({
         palabra: '',
         significado: '',
         ejemplos: ['','','']
     })
 
-     useEffect(() =>{
+    const onChangeHandler = (e) =>{
+        console.log('onChangeHandler')
+        const target = e.target
+        const value = target.value
+        const name = target.name
+        if(name === 'ejemplo1'){
+            let nuevoEjemplos = [...palabra.ejemplos]
+            nuevoEjemplos[0] = value
+            setPalabra({
+                ...palabra,
+                'ejemplos': nuevoEjemplos
+            })
+        }
+        if(name === 'ejemplo2'){
+            let nuevoEjemplos = [...palabra.ejemplos]
+            nuevoEjemplos[1] = value
+            setPalabra({
+                ...palabra,
+                'ejemplos': nuevoEjemplos
+            })
+        }
+        if(name === 'ejemplo3'){
+            let nuevoEjemplos = [...palabra.ejemplos]
+            nuevoEjemplos[2] = value
+            setPalabra({
+                ...palabra,
+                'ejemplos': nuevoEjemplos
+            })
+        }
+        if(name != 'ejemplo1' && name != 'ejemplo2' && name != 'ejemplo3'){
+            setPalabra({
+                ...palabra,
+                [name]: value
+            })
+        }
+    }
+
+    useEffect(() =>{
         const idCategoria = props.match.params.idCategory
         const palabrasUsuario = props.palabras[idCategoria]
         const idPalabra = props.match.params.idWord
@@ -41,8 +78,8 @@ const FormularioPalabraPage = (props) => {
         } else {
             palabra = getPalabra(palabrasUsuario, idPalabra)[0]
         }
-        setPalabra(palabra)
-     },[])
+    setPalabra(palabra)
+    },[])
 
     return (
         <div className="Formulario-Palabra">
@@ -62,17 +99,17 @@ const FormularioPalabraPage = (props) => {
                             <Col md={6} className='d-flex flex-column'>
                                 <FormGroup className='my-5'>
                                     <Label for="palabraForm">Palabra</Label>
-                                    <Input type='text' name="salabra" id="palabraForm" placeholder="Ingresa la palabra" value={palabra.palabra}/>
+                                    <Input type='text' name="palabra" id="palabraForm" placeholder="Ingresa la palabra" value={palabra.palabra} onChange={onChangeHandler}/>
                                 </FormGroup>
                                 <FormGroup className='my-5'>
                                     <Label for="sifnificadoForm">Significado</Label>
-                                    <Input type='text' name="significado" id="significadoForm" placeholder="Ingresa el significado" value={palabra.significado}/>
+                                    <Input type='text' name="significado" id="significadoForm" placeholder="Ingresa el significado" value={palabra.significado} onChange={onChangeHandler}/>
                                 </FormGroup>
                                 <FormGroup>
                                     <Label>Ejemplos</Label>
-                                    <Input type='text' name="ejemplo1" id="ejemplo1" placeholder="Ingresa un ejemplo" className="my-2" value={palabra.ejemplos[0]}/>
-                                    <Input type='text' name="ejemplo2" id="ejemplo2" placeholder="Ingresa un ejemplo" className="my-2"/>
-                                    <Input type='text' name="ejemplo3" id="ejemplo3" placeholder="Ingresa un ejemplo" className="my-2"/>
+                                    <Input type='text' name="ejemplo1" id="ejemplo1" placeholder="Ingresa un ejemplo" className="my-2" value={palabra.ejemplos[0] ? palabra.ejemplos[0]: null} onChange={onChangeHandler}/>
+                                    <Input type='text' name="ejemplo2" id="ejemplo2" placeholder="Ingresa un ejemplo" className="my-2" value={palabra.ejemplos[1] ? palabra.ejemplos[1]: null} onChange={onChangeHandler}/>
+                                    <Input type='text' name="ejemplo3" id="ejemplo3" placeholder="Ingresa un ejemplo" className="my-2" value={palabra.ejemplos[2] ? palabra.ejemplos[2]: null} onChange={onChangeHandler}/>
                                 </FormGroup>
                                 <Button className='align-self-end'>Aceptar</Button>
                          </Col>
