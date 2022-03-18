c7f2fb12e0853f121b606a939c2b0dcefd4eb539
Gerardo Villa <gerardovilla@foodology.com.co>
2022-03-18 00:11:17 -0600

    sign up design

--- src/pages/SignInPage/SignIn.view.tsx
+++ src/pages/SignInPage/SignIn.view.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './SignIn.scss';
-import { Icon } from '@mui/material';
+import { Icon, TextField } from '@mui/material';
 import Logo from "../../assets/images/logo_icon.png";
+import { width } from '@mui/system';
 
 interface SignInViewProps {
     goBackClick: () => void;
@@ -18,10 +19,58 @@ const SignInView = (props: SignInViewProps): React.ReactElement => {
                 <h1>Registrarse</h1>
             </div>
             <div className='rightSide'>
-
+                <div className='customRow'>
+                    <Input label={'Nombre'}/>
+                    <Input label={'Apellido'}/>
+                </div>
+                <div className='customRow'>
+                    <Input label={'Correo'} type={'email'}/>
+                </div>
+                <div className='customRow'>
+                    <Input label={'Contraseña'} type={'password'}/>
+                    <Input label={'Repetir contraseña'} type={'password'}/>
+                </div>
             </div>
         </div>
     )
+}
+
+
+interface InputProps {
+    label : string,
+    type? : string,
+}
+
+const Input = (props: InputProps): React.ReactElement => {
+    const { label, type } = props;
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const key = event.key;
+        if(key === 'Enter'){
+            if(inputRef.current){
+                inputRef.current.blur();
+            }
+        }
+    }
+
+    return (
+        <TextField
+            label={label} 
+            inputRef={inputRef} 
+            onKeyDown={handleKeyDown} 
+            InputProps={{
+                sx:{
+                    backgroundColor: 'white',
+                },
+            }}
+            sx={{
+                width: '100%',
+                margin: '1em'
+            }}
+            type={type}
+        />
+    );
 }
 
 export default SignInView
